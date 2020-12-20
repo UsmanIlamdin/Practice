8582e952fba0eb677f5f545afc6fa843556bdbbf
Usman IlamDin <55306553+UsmanIlamdin@users.noreply.github.com>
2020-12-20 09:28:35 -0800

    add functionality

--- solo.py
+++ solo.py
@@ -190,8 +190,200 @@ integer keys within a certain range.
 Dictionaries can be indexed in the same way 
 as lists, using square brackets containing 
 keys.
+Only immutable objects can be used as keys 
+to dictionaries. Immutable objects are those 
+that can't be changed. So far, the only 
+mutable objects you've come across are lists 
+and dictionaries. Trying to use a mutable 
+object as a dictionary key causes a TypeError.
 '''
 
 # ages = {'Name':'Usman' , 'F.Name': 'IlamDin' }
 
 # print(ages['Name'])
+
+# primary = {
+#     "red": [255, 0, 0], 
+#     "green": [0, 255, 0], 
+#     "blue": [0, 0, 255], 
+# }
+# print(primary["red"])
+# print(primary["red"][2])
+
+# squares = {1: 1, 2: 4, 3: "error", 4: 16,}
+# squares[8] = 64
+# squares[3] = 9
+# print(squares)
+# name = ['Name', 'usman', 'ilamdin']
+# print(name.index('Name'))
+
+# pairs = {1: "apple",
+#         "orange": [2, 3, 4], 
+#     True: False, 
+#     None: "True",
+# }
+
+# print(pairs.get("orange"))
+# print(pairs.get(7))  # By default return None
+# print(pairs.get(12345, "not in dictionary"))
+
+'''
+Tuples
+
+
+Tuples are very similar to lists, except that 
+they are immutable (they cannot be changed).
+Also, they are created using parentheses, 
+rather than square brackets.
+'''
+
+# list1 = [1,2,3,4]
+# dic   = {1:2, 2:5}
+# tup   = (1,2,3,4)  # we cannot reasign the tuple value
+
+
+'''
+List Slices
+
+
+List slices provide a more advanced way of 
+retrieving values from a list. Basic list 
+slicing involves indexing a list with two 
+colon-separated integers. This returns a new 
+list containing all the values in the old 
+list between the indices.
+'''
+# squares = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+# print(squares[1:3])
+# print(squares[:7])
+# print(squares[7:])
+# print(squares[2::6])
+
+
+'''
+List Comprehensions
+
+List comprehensions are a useful way of quickly 
+creating lists whose contents obey a simple rule.
+'''
+
+# lis = [i for i in range(10)]
+# print(lis)
+
+# string formatting
+# nums = [4, 5, 6]
+# msg = "Numbers: {} {} {}". format(nums[0], nums[1], nums[2])
+# print(msg)
+
+# name= 'USMAN'
+# print("Name it {} {} {}".format(name, name , name))
+
+# print('{c}{b}{d}'.format(a=10 , d= 13 , b = 5 , c= 4))
+
+# print(','.join(["spam" , "eggs" , "ham"]))
+# #prints "spam, eggs, ham"
+
+# print("Hello ME".replace("ME", "world"))
+# #prints "Hello world"
+
+# print("This is a sentence.".startswith("This"))
+# # prints "True"
+
+# print("This is a sentence.".endswith("sentence."))
+# # prints "True"
+
+# print("This is a sentence.".upper())
+# # prints "THIS IS A SENTENCE."
+
+# print("AN ALL CAPS SENTENCE".lower())
+# #prints "an all caps sentence"
+
+# print("spam ,eggs, ham".split(",")) # , shows it split here.
+# #prints "['spam', 'eggs', 'ham']"
+
+
+'''
+Useful functions in list
+'''
+# lis = [1,2,3,4,5,10,2,3]
+
+
+# all(lis)
+# if all(num  <= 10  for num in lis):
+#     print("All larger than 5")
+
+# if any([i % 2 == 0 for i in lis]):
+#     print("At least one is even")
+
+'''
+The function enumerate can be used to iterate 
+through the values and indices of a list 
+simultaneously.
+'''
+# for v in enumerate(lis):
+#     print(v)
+
+# def count_text(text , char):
+#     count = 0
+#     for c in text:
+#         if c == char:
+#             count += 1
+#     return count
+
+# filename = input('Enter file Name')
+
+# with open(filename) as file:
+#     text = file.read()
+
+# print(count_text(text, 'written'))
+
+
+'''
+Functional Programming
+
+
+Functional programming is a style of programming that 
+(as the name suggests) is based around functions.
+A key part of functional programming is higher-order 
+functions. We have seen this idea briefly in the previous 
+lesson on functions as objects. Higher-order functions 
+take other functions as arguments, or return them as results.
+'''
+# def apply_twice(func, arg):
+#     return func(func(arg))
+
+# def add_five(x):
+#     return x + 5
+# # add_five is passed and then mapped func which again call
+# print(apply_twice(add_five, 10))
+''' 
+Pure and Impure function 
+'''
+# Pure 
+# def pure_function(x, y):
+#   temp = x + 2*y
+#   return temp / (2*x + y)
+
+# #Impure functon
+# some_list = []
+
+# def impure(arg):
+#   some_list.append(arg)
+'''
+he same is possible with functions, provided that 
+they are created using lambda syntax. Functions 
+created this way are known as anonymous.
+This approach is most commonly used when passing a 
+simple function as an argument to another function. 
+The syntax is shown in the next example and consists 
+of the lambda keyword followed by a list of arguments, 
+a colon, and the expression to evaluate and return.
+'''
+# def my_func(f, arg):
+#   return f(arg)
+
+# my_func(lambda x: 2*x*x, 5)
+#(lambda x: 2*x*x )(10)
+# name = lambda x:2*x
+# print(name(10))
+
