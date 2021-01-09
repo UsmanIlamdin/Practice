bd202985457d36ab2d071a4a74b31ce6d6dda82a
Usman IlamDin <55306553+UsmanIlamdin@users.noreply.github.com>
2021-01-09 22:22:29 +0500

    add 2 more chapter

---
+++ Notes/conditions/cmp-operator.py
@@ -0,0 +1,13 @@
+'''
+python 2 function
+['Equal', 'Greater than','Less than',][cmp[x,y]
+'''
+
+
+
+'''
+first letter of each list member
+'''
+lst = ['alpha', 'bravo', 'charlie', 'delta', 'echo']
+for s in lst:
+    print(s[0:1])
